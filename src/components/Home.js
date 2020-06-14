5d2c7860fb7e0da14f863519455ca7d023420bba
magnubgr <magnus.bg@hotmail.com>
2020-06-14 09:52:03 +0200

    Added sass partials for the components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,22 @@ function Home() {
     <div className="Home">
 
       <p>Home</p>
+
+      <div className="blue">
+        <div className="blue__text">
+          Blue
+        </div>
+
+      </div>
+
+      <div className="otherBlue">
+        <div className="otherBlue__text">
+          Other Blue
+        </div>
+      </div>
+
+
+
       <p>Home</p>
       <p>Home</p>
       <p>Home</p>
