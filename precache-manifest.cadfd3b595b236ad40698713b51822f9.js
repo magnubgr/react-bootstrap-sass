c32599607156b931f55cf3c7680d42699f569220
magnubgr <magnus.bg@hotmail.com>
2020-06-14 09:59:22 +0200

    Updates

---
+++ precache-manifest.cadfd3b595b236ad40698713b51822f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e16239b4510034524cbfec924db49d",
+    "url": "/react-bootstrap-sass/index.html"
+  },
+  {
+    "revision": "8510f6961de71a3348c4",
+    "url": "/react-bootstrap-sass/static/css/main.7f5e95be.chunk.css"
+  },
+  {
+    "revision": "577fb48b794aec8f3a1d",
+    "url": "/react-bootstrap-sass/static/js/2.833fe66a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bootstrap-sass/static/js/2.833fe66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8510f6961de71a3348c4",
+    "url": "/react-bootstrap-sass/static/js/main.9ba51e21.chunk.js"
+  },
+  {
+    "revision": "a4505342511d4ef380c3",
+    "url": "/react-bootstrap-sass/static/js/runtime-main.a12dc64d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bootstrap-sass/static/media/logo.5d5d9eef.svg"
+  }
+]);
